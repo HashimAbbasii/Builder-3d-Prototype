7bceebe4f032ff385d4fde033920de3dad272aa4
Amna Hassani <hassaniamna9@gmail.com>
2024-09-06 11:40:34 +0500

    start panel

---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject startPanel;
+    void Start()
+    {
+        startPanel.SetActive(true);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
