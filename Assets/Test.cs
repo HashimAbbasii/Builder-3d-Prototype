7e4c33f3e699fa0622630aed4a23eccae2a60dd8
Omersaeed02 <omar.saeed@funavry.com>
2024-10-02 10:29:21 +0500

    Push

---
+++ Assets/Test.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Test : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            Debug.Log("OnMouseDown If");
+        }
+        else
+        {
+            Debug.Log("OnMouseDown Else");
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            Debug.Log("OnMouseUp If");
+        }
+        else
+        {
+            Debug.Log("OnMouseUp Else");
+        }
+    }
+    
+}
