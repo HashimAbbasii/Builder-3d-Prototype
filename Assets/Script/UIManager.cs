b053647bc50f4af609eebe39ef069493eb868c54
HashimAbbasii <94447861+HashimAbbasii@users.noreply.github.com>
2024-10-03 15:50:16 +0500

    push-PauseFunctionality

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+//using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class UIManager : MonoBehaviour
     public ModelSpawnButton modelButtonPrefab;
     
     public GameObject startPanel;
+    public GameObject GamePanelPause;
     
     private void Start()
     {
@@ -19,5 +21,26 @@ public class UIManager : MonoBehaviour
     public void HandleSceneManagement(int index)
     {
         SceneManager.LoadScene(index);
+    }
+
+
+    public void Return()
+    {
+        GamePanelPause.gameObject.SetActive(false);
+    }
+    public void Restart(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+    public void Exit()
+    {
+        // Quit the application
+#if UNITY_EDITOR
+        // If running in the Unity Editor
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            // If running in a standalone build
+            Application.Quit();
+#endif
     }
 }
