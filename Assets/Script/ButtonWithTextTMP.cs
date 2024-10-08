b562f1204e6ecd329671e6a0518cb51f4ec6e7a9
Omersaeed02 <omar.saeed@funavry.com>
2024-10-08 15:28:52 +0500

    Update ButtonWithTextTMP.cs

--- Assets/Script/ButtonWithTextTMP.cs
+++ Assets/Script/ButtonWithTextTMP.cs
@@ -1,9 +1,39 @@
 using TMPro;
+using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
 
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.UI;
+
+[CustomEditor(typeof(ButtonWithTextTMP), true)]
+[CanEditMultipleObjects]
+public class ButtonWithTextTMPEditor : ButtonEditor
+{
+    SerializedProperty m_TextProperty;
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        m_TextProperty = serializedObject.FindProperty("buttonText");
+    }
+    
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        EditorGUILayout.Space();
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(m_TextProperty, new GUIContent("Text"));
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+
+#endif
+
 public class ButtonWithTextTMP : Button
 {
+    
     public TMP_Text buttonText;
     
     protected override void Awake()
