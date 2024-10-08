bbf4721b343e6b67227d51e4c77c1755362d7dc9
Omersaeed02 <omar.saeed@funavry.com>
2024-10-08 12:14:01 +0500

    Push

--- Assets/CollectiveDistanceManager.cs
+++ Assets/CollectiveDistanceManager.cs
@@ -12,20 +12,22 @@ public class CollectiveDistanceManager : MonoBehaviour
 
     public ButtonWithTextTMP objectDistanceHandlerButton;
 
-    public Color textUnselectedColor = new Color(1f, 1f, 1f);
-    public Color textSelectedColor = new Color(0f, 0.533f, 0.494f);
+    // public Color textUnselectedColor = new Color(1f, 1f, 1f);
+    // public Color textSelectedColor = new Color(0f, 0.533f, 0.494f);
     
     private Color _currentTextColor;
 
     private void Start()
     {
-        _currentTextColor = textUnselectedColor;
+        _currentTextColor = ManagerHandler.Instance.uiManager.canvasHandler.textUnselectedColor;
         objectDistanceHandlerButton.buttonText.color = _currentTextColor;
     }
 
     public void ToggleObjectDistanceHandlerScript()
     {
-        _currentTextColor = _currentTextColor == textUnselectedColor ? textSelectedColor : textUnselectedColor;
+        _currentTextColor = _currentTextColor == ManagerHandler.Instance.uiManager.canvasHandler.textUnselectedColor ? 
+            ManagerHandler.Instance.uiManager.canvasHandler.textSelectedColor : 
+            ManagerHandler.Instance.uiManager.canvasHandler.textUnselectedColor;
         objectDistanceHandlerButton.buttonText.color = _currentTextColor;
         objectDistanceHandler.ToggleScript();
     }
--- Assets/Script/CanvasHandler.cs
+++ Assets/Script/CanvasHandler.cs
@@ -6,4 +6,7 @@ public class CanvasHandler : MonoBehaviour
 {
     public RectTransform furnitureButtonsContentHolder;
     public RectTransform evidenceButtonsContentHolder;
+    
+    public Color textUnselectedColor = new Color(1f, 1f, 1f);
+    public Color textSelectedColor = new Color(0f, 0.533f, 0.494f);
 }
