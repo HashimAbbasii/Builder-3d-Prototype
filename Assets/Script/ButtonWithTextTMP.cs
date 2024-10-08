c31d2d7a371110e46e1a867e8c20fee8b5013d01
Omersaeed02 <omar.saeed@funavry.com>
2024-10-08 16:02:18 +0500

    Update ButtonWithTextTMP.cs

--- Assets/Script/ButtonWithTextTMP.cs
+++ Assets/Script/ButtonWithTextTMP.cs
@@ -27,7 +27,48 @@ public class ButtonWithTextTMPEditor : ButtonEditor
         EditorGUILayout.PropertyField(m_TextProperty, new GUIContent("Text"));
         serializedObject.ApplyModifiedProperties();
     }
+    
+    [MenuItem("GameObject/UI/Button (WWT)", false, 10)]
+    public static void CreateButtonGameObject(MenuCommand menuCommand)
+    {
+        GameObject parent = menuCommand.context as GameObject;
+        
+        // Create a new GameObject
+        var buttonGO = new GameObject("Button (WTT)");
+        var rectTransform = buttonGO.AddComponent<RectTransform>();
+        rectTransform.sizeDelta = new Vector2(160, 30);
+        
+        var childText = new GameObject("Text (TMP)");
+        childText.AddComponent<RectTransform>();
+        childText.transform.SetParent(buttonGO.transform);
+
+        var image = buttonGO.AddComponent<Image>();
+        image.sprite = AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/UISprite.psd");
+        image.type = Image.Type.Sliced;
+        
+        var button = buttonGO.AddComponent<ButtonWithTextTMP>();
+        button.targetGraphic = image;
+        
+        var text = childText.AddComponent<TextMeshProUGUI>();
+        text.text = "Button (WTT)";
+        text.fontSize = 24;
+        text.color = Color.black;
+        text.alignment = TextAlignmentOptions.Center;
+        
+        var textRectTransform = childText.GetComponent<RectTransform>();
+        textRectTransform.anchorMin = Vector2.zero;
+        textRectTransform.anchorMax = Vector2.one;
+        textRectTransform.sizeDelta = Vector2.zero;
+
+        buttonGO.GetComponent<ButtonWithTextTMP>().buttonText = text;
+
+        buttonGO.transform.parent = parent.transform;
+        rectTransform.localScale = Vector3.one;
+        // Add the GameObject to the current selection
+        Selection.activeObject = buttonGO;
+    }
 }
+
 
 #endif
 
